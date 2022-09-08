1db874ace2da7d3701ad7a78e564d009618d4232
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2022-09-08 14:22:34 +0600

    json data created

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import './Home.css';
+
+const Home = () => {
+    const [product, setProduct] = useState([]);
+
+    useEffect(()=>{
+
+    },[])
+    return (
+        <div className='div-container'>
+            <div className="">
+
+            </div>
+            <div className="">
+
+            </div>
+        </div>
+    );
+};
+
+export default Home;
